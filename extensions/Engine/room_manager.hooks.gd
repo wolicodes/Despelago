ac91c54c5c215ce05a55507b37dfa103e193b46a
wolicodes <676095+wolicodes@users.noreply.github.com>
2026-07-01 02:31:37 +0200

    remove the tutorial nodes instead of hiding them

--- extensions/Engine/room_manager.hooks.gd
+++ extensions/Engine/room_manager.hooks.gd
@@ -5,7 +5,7 @@ func _ready(chain: ModLoaderHookChain) -> void:
 	var tutorial = main_node.get_node_or_null("Tutorial")
 	var tutorial_message = main_node.get_node_or_null("TutorialMessage")
 	if tutorial:
-		tutorial.hide()
+		tutorial.queue_free()
 	if tutorial_message:
-		tutorial_message.hide()
+		tutorial_message.queue_free()
 	chain.execute_next()
