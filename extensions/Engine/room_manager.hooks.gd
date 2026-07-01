b0a6e02b108584a0ceb6d34d1f9c3a089076dae8
wolicodes <676095+wolicodes@users.noreply.github.com>
2026-07-01 02:30:10 +0200

    also hide TutorialMessage for Room_05

--- extensions/Engine/room_manager.hooks.gd
+++ extensions/Engine/room_manager.hooks.gd
@@ -2,7 +2,10 @@ extends Object
 
 func _ready(chain: ModLoaderHookChain) -> void:
 	var main_node := chain.reference_object as Room
-	var tuto = main_node.get_node_or_null("Tutorial")
-	if tuto:
-		tuto.hide()
+	var tutorial = main_node.get_node_or_null("Tutorial")
+	var tutorial_message = main_node.get_node_or_null("TutorialMessage")
+	if tutorial:
+		tutorial.hide()
+	if tutorial_message:
+		tutorial_message.hide()
 	chain.execute_next()
