d760cc00a11f010cc9ed23ccf7f4feec69f8c6a0
wolicodes <676095+wolicodes@users.noreply.github.com>
2026-07-15 01:31:34 +0200

    dont give the collectible to the player upon collection, will be handled via AP

--- extensions/Levels/Components/bonus_collectable.gd
+++ extensions/Levels/Components/bonus_collectable.gd
@@ -11,3 +11,11 @@ func pickup():
 	collectable_icon_instance = collectable_icon_scene.instantiate()
 	level.player.add_child(collectable_icon_instance)
 	collectable_icon_instance.setup(is_first_collectable)
+
+# MOD dont register the collectible when collected and dont
+# show a notification for it
+func collect():
+	is_collected = true
+	collectable_icon_instance.queue_free()
+	#game_manager.add_collectable(id)
+	#level.hud.show_collectable_notification(is_first_collectable)
