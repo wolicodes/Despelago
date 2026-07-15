d07120e02cf55eac1df2125496e026c2f7d0567b
wolicodes <676095+wolicodes@users.noreply.github.com>
2026-07-15 17:29:14 +0200

    small cleanup

--- mod_main.gd
+++ mod_main.gd
@@ -5,8 +5,6 @@ const DESPELAGO_LOG_NAME := "Woli-Despelago:Main"
 
 var mod_dir_path := ""
 var extensions_dir_path := ""
-var translations_dir_path := ""
-
 
 func _init() -> void:
 	ModLoaderLog.info("Init", DESPELAGO_LOG_NAME)
@@ -17,12 +15,11 @@ func _init() -> void:
 
 func install_script_extensions() -> void:
 	extensions_dir_path = mod_dir_path.path_join("extensions")
-	
+	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Content/cinematic_intro.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Engine/game_manager.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Engine/achievements/achievements_manager.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Engine/level_manager.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Engine/hud_controller.gd"))
-
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Levels/Components/bonus_light.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Levels/Components/bonus_collectable.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Levels/Components/switch.gd"))
@@ -30,11 +27,11 @@ func install_script_extensions() -> void:
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Levels/Components/lamp_chandelier.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Levels/Menus/Map/map_node.gd"))
 	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Levels/Components/bonus_invite_balloon.gd"))
-	
-	ModLoaderMod.install_script_extension(extensions_dir_path.path_join("Content/cinematic_intro.gd"))
+
 
 func install_script_hook_files() -> void:
 	ModLoaderMod.install_script_hooks("res://Engine/room_manager.gd", "res://mods-unpacked/Woli-Despelago/extensions/Engine/room_manager.hooks.gd")
+
 
 func _ready() -> void:
 	ModLoaderLog.info("Ready", DESPELAGO_LOG_NAME)
